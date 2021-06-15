c0f50b513b44455fc77e9c2124767388d6498013
juaanzim <joaozulian0@gmail.com>
2021-06-15 05:46:23 -0300

    terceiro commit

--- BDG.py
+++ BDG.py
@@ -15,8 +15,11 @@ ims = [
 img = Image.new('RGBA', (400, 400), color = (randint(0,255),randint(0,255),randint(0,255)))
 draw = ImageDraw.Draw(img)
 
-def set_img():
-    img.paste(ims[randint(0,6)],(randint(0,290),randint(0,290)))
+def set_img(valor):
+    i = 0
+    while i <= valor - 1:
+        i = i+1
+        img.paste(ims[randint(0,6)],(randint(0,290),randint(0,290)))
 
 fnt = [
     ImageFont.truetype(f"{os.path.dirname(__file__)}\F\ARIAL.TTF",randint(60,60)),
@@ -35,9 +38,7 @@ xy=(
     (50,260)
 )
 
-set_img()
-set_img()
-set_img()
+set_img(4)
 
 draw.text((90,180), "Bom Dia", font=fnt[randint(0,7)], fill=(randint(0,250),randint(0,250),randint(0,250)),stroke_width=1)
 blurImage = img.filter(ImageFilter.BoxBlur(radius=randint(0,30)))
